7746ee52ff6b8cd90e6ad56e029bdd58154e320d
prakher singh <prakhersingh2345@gmail.com>
2026-05-03 02:11:25 +0530

    fix: add mongodb connection timeout

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const logger = require('../utils/logger');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      // Mongoose 8 no longer needs useNewUrlParser / useUnifiedTopology
+      serverSelectionTimeoutMS: 10000, // 10 seconds timeout
     });
 
     logger.info(`MongoDB connected: ${conn.connection.host}`);
